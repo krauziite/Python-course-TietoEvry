861b64e057ca445d351f0c4e99caa8bf925e2644
krauziite <58147986+krauziite@users.noreply.github.com>
2021-10-01 17:29:37 +0300

    Update day_4_classwork.py

--- day_4_classwork.py
+++ day_4_classwork.py
@@ -31,25 +31,18 @@
 
 
 
-# # Generating Triangle Shape
-# def triangleShape(n):
-#     for i in range(n):
-#         for j in range(n-i):
-#             print(' ', end=' ')
-#         for k in range(2*i+1):
-#             print('*',end=' ')
-#         print()
-#
-# # Generating Pole Shape
-# def poleShape(n):
-#     for i in range(n):
-#         for j in range(n-1):
-#             print(' ', end=' ')
-#         print('* * *')
-#
-# # Input and Function Call
-# row = int(input('Enter number of rows: '))
-#
-# triangleShape(row)
-# triangleShape(row)
-# poleShape(row)
+def christmas_tree(n):
+    z=n-1
+    x=1
+    for i in range(0,n):
+        for i in range(0,z):
+            print('',end='')
+        for i in range(0,x):
+            print('+',end='')
+        for i in range(0,z):
+            print('',end='')
+        x=x*2
+        x=x-1
+        z=z-1
+        print()
+christmas_tree(5)
